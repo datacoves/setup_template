6d00bfc986e237aefec222b8441ffc51657cb29b
Noel Gomez <gomezn@datacoves.com>
2025-04-20 12:32:44 -0700

    Fix formatting

--- dbt_setup_test.sh
+++ dbt_setup_test.sh
@@ -1,3 +1,3 @@
 export DATACOVES__REPO_PATH="/config/test_template/a_new_project"
 export DATACOVES__DBT_HOME="/config/test_template/a_new_project/transform"
-dbt-coves setup --update --template-url ./
+dbt-coves setup --template-url ./
